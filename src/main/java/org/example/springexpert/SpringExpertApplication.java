678f67d7b86bb5f4008ae1534d9ee9f1e48f6528
f-api <kisy32469@gmail.com>
2024-08-22 23:29:09 +0900

    init

---
+++ src/main/java/org/example/springexpert/SpringExpertApplication.java
@@ -0,0 +1,13 @@
+package org.example.springexpert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringExpertApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringExpertApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/springexpert/SpringExpertApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springexpert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class SpringExpertApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
